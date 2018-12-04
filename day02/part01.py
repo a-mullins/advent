f83c27a504f04caa2a3a3b3223be72619f05140b
Adam Mullins <adam.l.m@gmail.com>
2018-12-04 09:26:40 -0500

    change for line in fp.readlines() -> for line in fp

--- day02/part01.py
+++ day02/part01.py
@@ -3,7 +3,7 @@
 twos = 0
 threes = 0
 with open('input', 'r') as fp:
-    for line in fp.readlines():
+    for line in fp:
         repeats = list(map(lambda x: line.count(x), set(line)))
         if 2 in repeats: twos += 1
         if 3 in repeats: threes += 1
