ba7854818780faccc86e8e659784bbcc2b514447
Adam Mullins <adam.l.m@gmail.com>
2019-09-26 20:23:56 -0400

    refactor 2015/day05

--- 2015/day05/p01.py
+++ 2015/day05/p01.py
@@ -1,27 +1,14 @@
 #!/usr/bin/env python3
+import re
 from sys import stdin
 
 
 def is_nice(s):
-    VOWELS = 'aeiou'
-    vowel_cnt = 0
-    run_cnt = 0
-    naughty_substring = False
-
-    prev = s[0]
-    if prev in VOWELS:
-        vowel_cnt += 1
-    for cur in s[1:]:
-        if prev + cur in ['ab', 'cd', 'pq', 'xy']:
-            naughty_substring = True
-            break
-        if cur in VOWELS:
-            vowel_cnt += 1
-        if cur == prev:
-            run_cnt += 1
-        prev = cur
-
-    return run_cnt >= 1 and vowel_cnt >= 3 and not naughty_substring
+    naughties = ('ab', 'cd', 'pq', 'xy')
+    rule1 = len(re.findall(r'[aeiou]', s)) >= 3
+    rule2 = re.search(r'(.)\1', s)
+    rule3 = not any((sub in s for sub in naughties))
+    return bool(rule1 and rule2 and rule3)
 
 
-print(len([line for line in stdin.readlines() if is_nice(line)]))
+print(sum((1 for line in stdin.readlines() if is_nice(line))))
