b8ce6af8120854a67be8dda4352fe29e9ed91c51
Adam Mullins <adam.l.m@gmail.com>
2019-09-28 17:24:27 -0400

    add 2015/day10

---
+++ 2015/day10/p01.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import re
+
+
+def look_and_say(seq):
+    output = ''
+    runs = [group[0] for group in re.findall(r'((\d)\2*)', seq)]
+
+    for run in runs:
+        output += str(len(run)) + run[0]
+
+    return output
+
+
+if __name__ == '__main__':
+    puzzle = input()
+    for _ in range(40):
+        puzzle = look_and_say(puzzle)
+
+    print(len(puzzle))
