d382ad72b6f0e9976c2ee9f8cb4fc9dc93819d26
Adam Mullins <adam.l.m@gmail.com>
2019-10-01 03:16:20 -0400

    add 2015/day16

---
+++ 2015/day16/p02.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+from sys import stdin
+
+target_sue = {'children': 3,
+              'cats': 7,
+              'samoyeds': 2,
+              'pomeranians': 3,
+              'akitas': 0,
+              'vizslas': 0,
+              'goldfish': 5,
+              'trees': 3,
+              'cars': 2,
+              'perfumes': 1}
+
+if __name__ == '__main__':
+    for line in stdin:
+        this_sue = {}
+        match = True
+
+        for elem in line.partition(':')[2].split(','):
+            prop, val = elem.split(':')
+            this_sue[prop.strip()] = int(val)
+
+        for val in this_sue.keys():
+            if val in ['cats', 'trees']:
+                if this_sue[val] <= target_sue[val]:
+                    match = False
+            elif val in ['pomeranians', 'goldfish']:
+                if this_sue[val] >= target_sue[val]:
+                    match = False
+            else:
+                if this_sue[val] != target_sue[val]:
+                    match = False
+
+        if match:
+            print(line.partition(':')[0])
