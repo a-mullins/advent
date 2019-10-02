5a8c70ab9e63b23b2b9e53564a19d2e342108da0
Adam Mullins <adam.l.m@gmail.com>
2019-10-02 06:03:04 -0400

    add 2015/day18

---
+++ 2015/day18/p01.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+from sys import stdin
+
+
+class LightGrid:
+    def __init__(self, init=None):
+        if init:
+            self.g = init
+        else:
+            self.g = [[0 for col in range(100)] for row in range(100)]
+        self.size = len(self.g)
+
+    def __str__(self):
+        ret_val = ''
+        for row in self.g:
+            ret_val += ''.join((('#' if col else '.' for col in row)))
+            ret_val += '\n'
+        return(ret_val)
+
+    def step(self):
+        next_g = [[0 for col in range(self.size)] for row in range(self.size)]
+        for row in range(self.size):
+            for col in range(self.size):
+                # row 1 col 5 problems
+                row_range = range(row - 1 if row > 0 else 0,
+                                  row + 2 if row < self.size-1
+                                          else self.size) # NOQA
+                col_range = range(col - 1 if col > 0 else 0,
+                                  col + 2 if col < self.size-1
+                                          else self.size) # NOQA
+                live_neighbors = 0
+                for y in row_range:
+                    for x in col_range:
+                        if self.g[y][x] and not (y == row and x == col):
+                            live_neighbors += 1
+                # juuuust in case...
+                del y, x
+
+                if self.g[row][col]:  # if alive
+                    if live_neighbors in (2, 3):
+                        next_g[row][col] = 1
+                    else:
+                        next_g[row][col] = 0
+                else:                           # if dead
+                    if live_neighbors == 3:
+                        next_g[row][col] = 1
+                    else:
+                        next_g[row][col] = 0
+        # END for next_row in range ...
+        self.g = next_g
+
+
+if __name__ == '__main__':
+    init = [[1 if c == '#' else 0 for c in row.strip()] for row in stdin]
+    lg = LightGrid(init)
+
+    for _ in range(100):
+        lg.step()
+
+    print(sum((col for row in lg.g for col in row)))
