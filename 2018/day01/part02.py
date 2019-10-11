cee8f78938a46551bd630056bfe0cf448a942202
Adam Mullins <adam.l.m@gmail.com>
2019-10-11 11:32:28 -0400

    [minor] remove comments

--- 2018/day01/part02.py
+++ 2018/day01/part02.py
@@ -2,11 +2,11 @@
 import sys
 from itertools import cycle
 
-with open(sys.argv[1], 'r') as fp:
+with open(sys.argv[1], 'rt') as fp:
     input_lines = fp.readlines()
 
 acc = 0
-freqs = set()  # search on sets is MUCH faster than lists
+freqs = set()
 freqs.add(acc)
 for line in cycle(input_lines):
     acc += int(line)
