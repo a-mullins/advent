d9d3e4c682ad2153ba4191dccf7bee3d2f0885bd
Adam Mullins <adam.l.m@gmail.com>
2022-06-02 12:28:38 -0400

    solve 2015/day24

---
+++ 2015/day24/p01.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+from itertools import combinations
+from functools import reduce
+import operator
+import sys
+
+
+def div_packages(packages, target_bins, target_mass, parts=None):
+    if not parts:
+        parts = target_bins
+    for run_len in range(1, len(packages)):
+        for group in (
+            x for x in combinations(packages, run_len) if sum(x) == target_mass
+        ):
+            if parts == 2:
+                return True
+            elif parts < target_bins:
+                return div_packages(
+                    list(set(packages) - set(group)),
+                    target_bins,
+                    target_mass,
+                    parts - 1,
+                )
+            elif div_packages(
+                list(set(packages) - set(group)),
+                target_bins,
+                target_mass,
+                parts - 1,
+            ):
+                return reduce(operator.mul, group, 1)
+
+
+if __name__ == "__main__":
+    packages = [int(x) for x in sys.stdin]
+    bins = int(sys.argv[1])
+
+    print(
+        div_packages(
+            packages, target_bins=bins, target_mass=sum(packages) / bins
+        )
+    )
