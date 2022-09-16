1fc072449e06f6afdd496ac8df3a20feb61a36ed
Adam Mullins <adam.l.m@gmail.com>
2022-09-16 01:08:03 -0400

    clean up 2016/day02/p02

--- 2016/day02/p02.js
+++ 2016/day02/p02.js
@@ -19,14 +19,14 @@ input = input.split('\n').filter( line => line != '' );
 for( let line of input ) {
     for( let ch of line ) {
         let tmp_row = row, tmp_col = col;
-        if(ch == "L") {if( keypad[tmp_row][--tmp_col] != ' ' )
-                       {row = tmp_row; col = tmp_col;}}
-        if(ch == "R") {if( keypad[tmp_row][++tmp_col] != ' ' )
-                       {row = tmp_row; col = tmp_col;}}
-        if(ch == "U") {if( keypad[--tmp_row][tmp_col] != ' ' )
-                       {row = tmp_row; col = tmp_col;}}
-        if(ch == "D") {if( keypad[++tmp_row][tmp_col] != ' ' )
-                       {row = tmp_row; col = tmp_col;}}
+        if(ch == "L") {--tmp_col;}
+        if(ch == "R") {++tmp_col;}
+        if(ch == "U") {--tmp_row;}
+        if(ch == "D") {++tmp_row;}
+        if( keypad[tmp_row][tmp_col] && keypad[tmp_row][tmp_col] != ' ' ) {
+            row = tmp_row;
+            col = tmp_col;
+        }
     }
     code += keypad[row][col];
 }
