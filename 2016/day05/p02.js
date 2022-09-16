4ab989c23b7527bd43d84ec03396a784e9d9ff68
Adam Mullins <adam.l.m@gmail.com>
2022-09-16 05:03:48 -0400

    solve 2016/day04 and day05

---
+++ 2016/day05/p02.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+"use strict";
+var fs = require("fs");
+var crypto = require("crypto");
+
+var input = fs.readFileSync(0, 'ascii').trim(); // 0 is stdin.
+var password = Array(8).fill(null);
+var num_found = 0;
+var index = 0;
+
+//while( password.length < 8 ) {
+while( num_found < 8 ) {
+    let next = crypto.createHash('md5').update(input + String(index))
+                     .digest('hex');
+    if( next.slice(0, 5) == '00000' && Number(next[5]) < 8
+        && !password[next[5]] ) {
+        password[ next[5] ] = next[6];
+        console.log(input+String(index), next,
+                    password.map( c => c ? c : '_' ).join(''));
+        num_found++;
+    }
+    //console.log(input + String(index), next);
+    index++;
+}
