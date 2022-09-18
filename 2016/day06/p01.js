4ce90fba2097c3c43fe7d5dc8aaaa8ca05ff0770
Adam Mullins <adam.l.m@gmail.com>
2022-09-18 19:38:42 -0400

    [minor] code cleanup

--- 2016/day06/p01.js
+++ 2016/day06/p01.js
@@ -3,7 +3,8 @@
 const fs = require("fs");
 
 
-let codes = fs.readFileSync(0, 'ascii').split('\n').map( line => line.split('') ).filter(line => line.length > 0);
+let input = fs.readFileSync(0, 'ascii').split('\n');
+let codes = input.map( line => line.split('') ).filter(line => line.length > 0);
 let counts = Array.from({length: codes[0].length}, () => new Map());
 let max_message = "";
 let min_message = "";
@@ -20,14 +21,9 @@ for(const col of counts) {
         cur > prev ? cur : prev);
     let min_count = [...col.values()].reduce((prev, cur) =>
         cur < prev ? cur : prev);
-    max_message += [...col].filter( elem => elem[1] == max_count )[0][0];
-    min_message += [...col].filter( elem => elem[1] == min_count )[0][0];
+    max_message += [...col].find( elem => elem[1] == max_count )[0];
+    min_message += [...col].find( elem => elem[1] == min_count )[0];
 }
-// TODO sort by val and select top key, or do it as we've done here
-//      O(n log n) for mergesort or, eh, O(2n)? Investigate.
 
 console.log("Max-count Message: %s\nMin-count Message: %s",
             max_message, min_message);
-
-// TODO what is the difference between var / let / const, esp. in terms of
-//      scoping?
