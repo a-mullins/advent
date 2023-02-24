8b18a46420e8f79ff29e4dcd9938dfee0fbd4eff
Adam Mullins <adam.l.m@gmail.com>
2023-02-24 05:08:39 -0500

    cleanup

--- 2016/day02/p02.js
+++ 2016/day02/p02.js
@@ -1,29 +1,31 @@
 #!/usr/bin/env node
 "use strict";
+const fs = require("fs");
 
-var keypad =
-    [ "       ",
-      "   1   ",
-      "  234  ",
-      " 56789 ",
-      "  ABC  ",
-      "   D   ",
-      "       "];
-var row = 3, col = 1;
-var code = ""
 
-var fs = require("fs");
-var input = fs.readFileSync(0, 'ascii'); // 0 is stdin.
-input = input.split('\n').filter( line => line != '' );
+const keypad = ["       ",
+                "   1   ",
+                "  234  ",
+                " 56789 ",
+                "  ABC  ",
+                "   D   ",
+                "       "];
+let row = 3,
+    col = 1,
+    code = "";
 
-for( let line of input ) {
-    for( let ch of line ) {
-        let tmp_row = row, tmp_col = col;
-        if(ch == "L") {--tmp_col;}
-        if(ch == "R") {++tmp_col;}
-        if(ch == "U") {--tmp_row;}
-        if(ch == "D") {++tmp_row;}
-        if( keypad[tmp_row][tmp_col] && keypad[tmp_row][tmp_col] != ' ' ) {
+const input = fs
+      .readFileSync(0, 'ascii').split('\n').filter( line => line != '' );
+
+for(const line of input) {
+    for(const c of line) {
+        let tmp_row = row,
+            tmp_col = col;
+        if(c === "L") {--tmp_col;}
+        if(c === "R") {++tmp_col;}
+        if(c === "U") {--tmp_row;}
+        if(c === "D") {++tmp_row;}
+        if(keypad[tmp_row][tmp_col] !== ' ') {
             row = tmp_row;
             col = tmp_col;
         }
