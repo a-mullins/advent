ddfb1d5bf5136ef5dd13036a7124b32ab5e1c972
Adam Mullins <adam.l.m@gmail.com>
2023-02-24 04:24:22 -0500

    cleanup

--- 2016/day02/p01.js
+++ 2016/day02/p01.js
@@ -1,24 +1,23 @@
 #!/usr/bin/env node
 "use strict";
+const fs = require("fs");
 
-var fs = require("fs");
-var input = fs.readFileSync(0, 'ascii'); // 0 is stdin.
-input = input.split('\n').filter( line => line != '' );
 
-var cur = 5;
-var left   = [1, 4, 7];
-var right  = [3, 6, 9];
-var top    = [1, 2, 3];
-var bottom = [7, 8, 9];
+const input  = fs.readFileSync(0, 'utf8').split('\n').filter(line => line != ''),
+      left   = [1, 4, 7],
+      right  = [3, 6, 9],
+      top    = [1, 2, 3],
+      bottom = [7, 8, 9];
 
-var code = "";
+let code = "",
+    cur = 5;
 
-for( let line of input ) {
-    for( let ch of line ) {
-        if(ch == "L") { if( !left.includes(cur) ) { cur--; } }
-        if(ch == "R") { if( !right.includes(cur) ) { cur++; } }
-        if(ch == "U") { if( !top.includes(cur) ) { cur -= 3; } }
-        if(ch == "D") { if( !bottom.includes(cur) ) { cur += 3; } }
+for(const line of input) {
+    for(const c of line) {
+        if(c === "L") {if(!left.includes(cur))   {cur--;}}
+        if(c === "R") {if(!right.includes(cur))  {cur++;}}
+        if(c === "U") {if(!top.includes(cur))    {cur -= 3;}}
+        if(c === "D") {if(!bottom.includes(cur)) {cur += 3;}}
     }
     code += String(cur);
 }
