d028551756474c2e33c36ceefcc8d6b8786fb019
Adam Mullins <adam.l.m@gmail.com>
2023-02-24 04:24:22 -0500

    cleanup

--- 2016/day01/p01.js
+++ 2016/day01/p01.js
@@ -1,32 +1,27 @@
 #!/usr/bin/env node
 "use strict";
+const fs = require("fs");
 
-var fs = require("fs");
-var input = fs.readFileSync(0, 'ascii'); // 0 is stdin.
-input = input.split(', ');
 
-var facing = 0; // 0 = N, 1 = E, 2 = S, 3 = W;
-function turnL() { if( --facing < 0 ) { facing = 3 } }
-function turnR() { if( ++facing > 3 ) { facing = 0 } }
+let facing = 0; // 0 = N, 1 = E, 2 = S, 3 = W;
+function turnL() {if(--facing < 0) {facing = 3}}
+function turnR() {if(++facing > 3) {facing = 0}}
 
-var x = 0, y = 0;
+let input = fs.readFileSync(0, 'ascii').split(', '), // 0 is stdin.
+    x = 0,
+    y = 0;
 
-input.forEach( elem => {
-    if( elem[0] == "L" ) { turnL(); }
-    if( elem[0] == "R" ) { turnR(); }
+for(const elem of input) {
+    if(elem[0] === "L") {turnL();}
+    if(elem[0] === "R") {turnR();}
 
     let dist = Number(elem.substr(1));
 
-    switch( facing ) {
-    case 0:
-        y += dist; break;
-    case 1:
-        x += dist; break;
-    case 2:
-        y -= dist; break;
-    case 3:
-        x -= dist; break;
-    }
-});
+    // Smaller than switch statement.
+    if(facing === 0) {y+=dist;}
+    if(facing === 1) {x+=dist;}
+    if(facing === 2) {y-=dist;}
+    if(facing === 3) {x-=dist;}
+};
 
-console.log( Math.abs(x) +  Math.abs(y) );
+console.log(Math.abs(x) + Math.abs(y));
--- 2016/day01/p02.js
+++ 2016/day01/p02.js
@@ -1,49 +1,40 @@
 #!/usr/bin/env node
 "use strict";
+const fs = require("fs");
 
-var fs = require("fs");
-var input = fs.readFileSync(0, 'ascii'); // 0 is stdin.
-input = input.split(', ');
 
-var facing = 0; // 0 = N, 1 = E, 2 = S, 3 = W;
-function turnL() { if( --facing < 0 ) { facing = 3 } }
-function turnR() { if( ++facing > 3 ) { facing = 0 } }
+let facing = 0; // 0 = N, 1 = E, 2 = S, 3 = W;
+function turnL() {if(--facing < 0) {facing = 3}} 
+function turnR() {if(++facing > 3) {facing = 0}}
 
-var x = 0, y = 0;
+let input = fs.readFileSync(0, 'ascii').split(', '), // 0 is stdin.
+    dup,
+    x = 0,
+    y = 0;
+const visited = [];
 
-var visited = [];
-
-function range(start, end) {
-    if( !end ) { end = start, start = 0;}
-    if( start == end ) { return [] }
-    else { return [start].concat(range(start+1, end)) }
-}
-
-var dup = undefined;
-
-for( elem of input ) {
-    if( elem[0] == "L" ) { turnL(); }
-    if( elem[0] == "R" ) { turnR(); }
+for(const elem of input) {
+    if(elem[0] === "L") {turnL();}
+    if(elem[0] === "R") {turnR();}
 
     let dist = Number(elem.substr(1));
 
-    for(i of range(dist)) {
-        if( facing == 0 ) { y++; }
-        if( facing == 1 ) { x++; }
-        if( facing == 2 ) { y--; }
-        if( facing == 3 ) { x--; }
+    for(let i = 0; i<dist; i++){
+        if(facing === 0) {y++;}
+        if(facing === 1) {x++;}
+        if(facing === 2) {y--;}
+        if(facing === 3) {x--;}
 
-        cur = String(x) + " " + String(y);
+        const cur = `${String(x)} ${String(y)}`;
         if(visited.includes(cur)) {
             dup = cur;
             visited.push(cur);
-            console.log('dup found: ' + cur +
-                        '\ndist: ' + (Math.abs(x) +  Math.abs(y)));
+            console.log(
+                `dup found: ${cur}\ndist: ${Math.abs(x) +  Math.abs(y)}`);
             break;
         } else {
             visited.push(cur);
         }
     }
-
-    if( dup ) { break; }
+    if(dup) {break;}
 }
