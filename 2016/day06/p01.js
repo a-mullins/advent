c2f1ac6bffaa3ce9fc91250580ea667cac159dcb
Adam Mullins <adam.l.m@gmail.com>
2023-02-26 11:17:36 -0500

    cleanup

--- 2016/day06/p01.js
+++ 2016/day06/p01.js
@@ -3,8 +3,9 @@
 const fs = require("fs");
 
 
-let input = fs.readFileSync(0, 'ascii').split('\n');
-let codes = input.map( line => line.split('') ).filter(line => line.length > 0);
+const codes = fs.readFileSync(0, 'ascii').split('\n')
+    .map(line => line.split('')).filter(line => line.length > 0);
+// TODO: Document next line.
 let counts = Array.from({length: codes[0].length}, () => new Map());
 let max_message = "";
 let min_message = "";
@@ -12,17 +13,17 @@ let min_message = "";
 for(let row = 0; row < codes.length; row++) {
     for(let col = 0; col < counts.length; col++) {
         let letter = codes[row][col];
-        counts[col].set( letter, counts[col].get(letter)+1 || 1 );
+        counts[col].set(letter, counts[col].get(letter)+1 || 1);
     }
 }
 
 for(const col of counts) {
-    let max_count = [...col.values()].reduce((prev, cur) =>
-        cur > prev ? cur : prev);
-    let min_count = [...col.values()].reduce((prev, cur) =>
-        cur < prev ? cur : prev);
-    max_message += [...col].find( elem => elem[1] == max_count )[0];
-    min_message += [...col].find( elem => elem[1] == min_count )[0];
+    const max_count = [...col.values()]
+          .reduce((prev, cur) => cur > prev ? cur : prev);
+    const min_count = [...col.values()]
+          .reduce((prev, cur) => cur < prev ? cur : prev);
+    max_message += [...col].find(elem => elem[1] == max_count)[0];
+    min_message += [...col].find(elem => elem[1] == min_count)[0];
 }
 
 console.log("Max-count Message: %s\nMin-count Message: %s",
