6480ce1c53fa333b9667726c66790d07cb914a51
Adam Mullins <adam.l.m@gmail.com>
2023-02-28 11:16:20 -0500

    cleanup

--- 2016/day10/p01.js
+++ 2016/day10/p01.js
@@ -1,89 +1,72 @@
 #!/usr/bin/env node
 "use strict";
-
-var fs = require("fs");
-
-
-var DEBUG = process.argv.includes('-d');
+const fs = require("fs");
 
 
 // lines have the form of either:
 // value N goes to bot B
 // bot B gives low to (bot|output) L and high to (bot|output) H
-var lines  = fs.readFileSync(0, 'ascii').trim().split('\n');
-var rules  = lines.filter(line => line.includes('gives'));
-rules = rules.map(rule => rule.split(' '));
-var inputs = lines.filter(line => line.includes('goes'));
-inputs = inputs.map(inp => inp.split(' '));
-var bots = [];
-var bins = [];
+const lines  = fs.readFileSync(0, "ascii").trim().split('\n');
+const rules  = lines
+      .filter(line => line.includes("gives"))
+      .map(rule => rule.split(' '));
+const inputs = lines
+      .filter(line => line.includes("goes"))
+      .map(inp => inp.split(' '));
+const bots = [];
+const bins = [];
 
-for(let parts of rules) {
+for(const parts of rules) {
     bots.push({num: Number(parts[1]),
                low_target: Number(parts[6]),
                low_type: parts[5],
                high_target: Number(parts[11]),
                high_type: parts[10],
                chips: []});
-    if(parts[5] == 'output') {
+    if(parts[5] == "output") {
         bins.push({num: Number(parts[6]),
                    chips: []});
     }
-    if(parts[10] == 'output') {
+    if(parts[10] == "output") {
         bins.push({num: Number(parts[11]),
                    chips: []});
     }
 }
 
-for( let parts of inputs ) {
-    let bot  = bots.find( bot => bot.num == Number(parts[5]));
+for(const parts of inputs) {
+    const bot = bots.find((bot) => bot.num == Number(parts[5]));
     if(bot.chips.length < 2) {
-        if(DEBUG) console.log("chip %d => bot %d",
-                              Number(parts[1]), bot.num);
         bot.chips.push(Number(parts[1]));
     } else {
-        if(DEBUG) console.log("bot %d full, can't add chip %s",
-                              bot.num, parts[1]);
         inputs.push(parts);
     }
 
     let candidates = [bot];
-    for( let cur of candidates ) {
+    for(const cur of candidates) {
         if(cur.chips.length >= 2) {
-            let [low, high] = cur.chips.sort( (a,b) => a-b );
+            const [low, high] = cur.chips.sort((a,b) => a-b);
             cur.chips = [];
             if(low == 17 && high == 61) {
-                console.log("bot %d handled 17 and 61", cur.num);
+                console.log(`bot ${cur.num} handled 17 and 61`);
             }
-            if(DEBUG) {console.log("bot %d has chips %d, %d", cur.num,
-                                   low, high);}
-
-            if(cur.low_type == 'output') {
-                let bin = bins.find( bin => bin.num == cur.low_target );
+            if(cur.low_type == "output") {
+                const bin = bins.find( bin => bin.num == cur.low_target );
                 bin.chips.push(low);
-                if(DEBUG) {console.log("giving chip %d to bin %d",
-                                       low, cur.low_target);}
             }
-            if(cur.high_type == 'output') {
-                let bin = bins.find( bin => bin.num == cur.high_target );
+            if(cur.high_type == "output") {
+                const bin = bins.find( bin => bin.num == cur.high_target );
                 bin.chips.push(high);
-                if(DEBUG) {console.log("giving chip %d to bin %d",
-                                       high, cur.high_target);}
             }
-            if(cur.low_type == 'bot') {
-                let target = bots.find( b => b.num == cur.low_target );
+            if(cur.low_type == "bot") {
+                const target = bots.find( b => b.num == cur.low_target );
                 target.chips.push(low);
-                if(DEBUG) {console.log("giving chip %d to bot %d",
-                                      low, target.num);}
                 if(target.chips.length >= 2) {
                     candidates.push(target);
                 }
             }
-            if(cur.high_type == 'bot') {
-                let target = bots.find( b => b.num == cur.high_target );
+            if(cur.high_type == "bot") {
+                const target = bots.find( b => b.num == cur.high_target );
                 target.chips.push(high);
-                if(DEBUG) {console.log("giving chip %d to bot %d",
-                                       high, target.num);}
                 if(target.chips.length >= 2) {
                     candidates.push(target);
                 }
@@ -92,6 +75,8 @@ for( let parts of inputs ) {
     }
 }
 
-var output = bins.filter(bin => [0, 1, 2].includes(bin.num))
-output = output.map(bin => bin.chips[0]).reduce((prev,cur) => prev*cur, 1);
-console.log("multiplied vals of 0, 1, and 2: %d", output);
+const output = bins
+      .filter(bin => 0 <= bin.num && bin.num <= 2)
+      .map(bin => bin.chips[0])
+      .reduce((prev,cur) => prev*cur, 1);
+console.log(`multiplied vals of 0, 1, and 2: ${output}`);
