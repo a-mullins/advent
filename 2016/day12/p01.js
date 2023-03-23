9c2887c10242dc87d60d2bb8975c70a74dcf11dd
Adam Mullins <adam.l.m@gmail.com>
2023-03-23 06:01:23 -0400

    cleanup backported from day23

--- 2016/day12/p01.js
+++ 2016/day12/p01.js
@@ -1,44 +1,54 @@
 #!/usr/bin/env node
 const fs = require("fs");
+const process = require("process");
 
 
 if(process.argv.includes("-D")) {var DEBUG = true;}
 
-r = {"a": 0, "b": 0, "c": 0, "d": 0};
+const r = {"a": 0, "b": 0, "c": 0, "d": 0};
 if(process.argv[1].includes("p02")) {
     if(DEBUG) {console.log("Called as p02");}
     r["c"] = 1;
 }
 
-text = fs.readFileSync(0, "ascii")
-    .split("\n")
-    .map(line => line.split(" "));
+const text = fs.readFileSync(0, "ascii")
+      .split("\n")
+      .filter(line => line.length > 0)
+      .map(line => line.split(" "));
 
 // ip = instruction pointer, program counter, &c.
 for(let ip = 0; 0 <= ip && ip < text.length; /*nop*/) {
-    instr = text[ip];
+    const instr = text[ip];
     if(DEBUG) {
         console.log(`ip = ${ip}; instr = ${instr.join(" ")};\n`
                     + ["a", "b", "c", "d"]
-                    .map(label => `r${label}: ${r[label]}`).join(', '));
+                    .map(label => `r${label}: ${r[label]}`).join(", "));
     }
+
     switch (instr[0]) {
     case "cpy":
-        let literal = Number(instr[1]); // could be NaN
-        if(literal) {
-            r[instr[2]] = literal;
-            if(DEBUG) {
-                console.log(`\tcpy lit ${literal} → ${instr[2]}`);
-            }
-        } else {
+        // second arg must be register name. if it's not, nop.
+        if(!(instr[2] in r)) {
+            if(DEBUG) {console.log(`\tbad cpy dest, skipping.`);}
+            ip++;
+            break;
+        }
+
+        if(isNaN(instr[1])) { // if src is NaN, it's a register label
             r[instr[2]] = r[instr[1]];
             if(DEBUG) {
-                console.log(`\tcpy reg ${instr[1]} (${r[instr[1]]}) → ${instr[2]}`);
+                console.log(`\tcpy reg ${instr[1]}`
+                            + `(${r[instr[1]]}) → ${instr[2]}`);
+            }
+        } else {
+            r[instr[2]] = Number(instr[1]);
+            if(DEBUG) {
+                console.log(`\tcpy lit ${instr[1]} → ${instr[2]}`);
             }
         }
         ip++;
         break;
-        
+
     case "inc":
         r[instr[1]]++;
         if(DEBUG) {
@@ -46,7 +56,7 @@ for(let ip = 0; 0 <= ip && ip < text.length; /*nop*/) {
         }
         ip++;
         break;
-        
+
     case "dec":
         r[instr[1]]--;
         if(DEBUG) {
@@ -54,27 +64,81 @@ for(let ip = 0; 0 <= ip && ip < text.length; /*nop*/) {
         }
         ip++;
         break;
-        
+
     case "jnz":
-        if(r[instr[1]] !== 0) {
-            ip += Number(instr[2]);
+        // jnz x y
+        // x must be register label, y must be integer.
+        let val = instr[1] in r
+            ? r[instr[1]]
+            : Number(instr[1]);
+
+        let dist = instr[2] in r
+            ? r[instr[2]]
+            : Number(instr[2]);
+
+        if(val !== 0) {
+            ip += dist;
             if(DEBUG){
-                console.log(`\tjnz ${instr[1]} = ${r[instr[1]]}; now ip = ${ip}`);
+                console.log(`\tjnz ${val} ${dist}; now ip = ${ip}`);
             }
         } else {
             ip++;
             if(DEBUG) {
-                console.log(`\tjnz ${instr[1]} = `
-                            + `${r[instr[1]]}; no jump, ip advanced`);
+                console.log(`\tjnz ${val} ${dist}; no jump, ip advanced`);
             }
         }
         break;
 
-    default:
+    case "tgl":
+        let i = ip + Number(r[instr[1]]);
+        let target = text[ip + Number(r[instr[1]])];
+
+        if(DEBUG) {
+            console.log(`\ttgl ${instr[1]},`
+                        + ` r${instr[1]} = ${r[instr[1]]}; `);
+        }
+        if(i < 0 || i >= text.length) {
+            if(DEBUG) { console.log("\ttgl index out of range, nop"); }
+            ip++;
+            break;
+        }
+        if(DEBUG) {
+            console.log(`\ttarget: text[${i}] = ${target.join(" ")}`);
+        }
+
+        // 1-arg
+        if(target.length === 2) {
+            if(target[0]=== "inc") {
+                target[0] = "dec";
+            } else {
+                target[0] = "inc";
+            }
+        }
+        // 2-arg
+        if(target.length === 3) {
+            if(target[0] === "jnz") {
+                target[0] = "cpy";
+            } else {
+                target[0] = "jnz";
+            }
+        }
+
+        if(DEBUG) {
+            console.log(`\ttarget: text[${i}] = ${target.join(" ")}`);
+        }
         ip++;
+        break;
+
+    default:
+        console.log("ERR: Unrecognized opcode. Quitting.");
+        console.log(`ERR: ip: ${ip}; text[${ip}]: ${instr.join(" ")}`);
+        process.exit(1);
     }
+    if(DEBUG) {console.log("");}
 }
 
 if(DEBUG) {console.log("");}
-console.log("Program terminated.\n\t" + 
-    ["a", "b", "c", "d"].map(label => `r${label}: ${r[label]}`).join(', '));
+console.log("Program halts.");
+console.log(
+    ["a", "b", "c", "d"].map(label => `r${label}: ${r[label]}`).join(", ")
+);
