8c5b1258b7a626e2e4870f76a08d6d687f39d4e7
Adam Mullins <adam.l.m@gmail.com>
2023-06-22 09:55:56 -0400

    remove symlinks in 2016/day12, factor out common code

--- 2016/day12/p02.mjs
+++ 2016/day12/p02.mjs
@@ -1,1 +1,23 @@
-p01.mjs
+#!/usr/bin/env node
+import fs from "node:fs";
+import process from "node:process";
+import { fileURLToPath } from "node:url";
+
+import { opt, run } from "./common.mjs";
+
+
+if (process.argv[1] === fileURLToPath(import.meta.url)) {
+    if(process.argv.includes("-D")) {var DEBUG = true;}
+
+    const text =
+          opt(fs.readFileSync(0, "ascii"))
+          .split("\n")
+          .filter(line => line.length > 0)
+          .map(line => line.split(" "));
+    
+    const r = run(text, {c: 1}, DEBUG);
+    
+    console.log(
+        ["a", "b", "c", "d"].map(label => `r${label}: ${r[label]}`).join(", ")
+    );
+}
