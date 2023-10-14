6714ca34c4ec00865f433fd5d25e2be787e4b30d
Adam Mullins <adam.l.m@gmail.com>
2023-10-14 12:46:26 -0400

    [minor] cleanup of 2017/day03/p01

--- 2017/day03/p01.c
+++ 2017/day03/p01.c
@@ -3,24 +3,18 @@
 #include <stdlib.h>
 
 int dist(int target) {
-    // square root of the bottom-right corner of target's square. eg:
-    //          …  …
-    // …  7  8  9 10
-    // … 22 23 24 25       sqrt_corner_above of 23 is 5, of 7 is 3.
+    //          …  …  square root of the bottom-right corner
+    // …  7  8  9 10      of target's 'ring'. eg:
+    // … 22 23 24 25  sqrt_corner_above of 23 is 5, of 7 is 3.
     int sqrt_corner_above = (int)ceilf(sqrtf((float)target));
-    if(!(sqrt_corner_above % 2)) { // is even
-        sqrt_corner_above++;
-    }
+    if(!(sqrt_corner_above % 2)) { sqrt_corner_above++; }  // +1 if even
     
     int      x =   (sqrt_corner_above-1)/2;
     int      y = -((sqrt_corner_above-1)/2);
-    int square =   (sqrt_corner_above-1)/2;
-    int  cur = sqrt_corner_above * sqrt_corner_above;
-
+    int    cur = sqrt_corner_above * sqrt_corner_above;
     enum directions {LEFT, RIGHT, UP, DOWN, LEN};
     enum directions dir = LEFT;
     int turn_at = cur - sqrt_corner_above + 1;
-
     while(cur != target && cur >= 0) {
         switch(dir) {
         case LEFT:  x--; break;
@@ -42,7 +36,7 @@ int main(void) {
     char *buf = NULL;
     getline(&buf, &buf_size, stdin); // implicit malloc.
     int i = atoi(buf);
-    free(buf);
+    free(buf);                       // explicit free.
     
     printf("%d\n", dist(i));
     return 0;
