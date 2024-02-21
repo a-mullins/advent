30326bfbd26754f8b0b8f0bab6d853c4db041d0f
Adam Mullins <adam.l.m@gmail.com>
2024-02-21 05:35:27 -0500

    [minor] cleanup day04

--- 2017/day04/p02.c
+++ 2017/day04/p02.c
@@ -6,27 +6,29 @@
 #define WORD_LIMIT 32
 
 int charcmp(const void *p, const void *q) {
-    return *(const char *)p - *(const char *)q;
+    const char a = *(const char *)p;
+    const char b = *(const char *)q;
+    return (a > b) - (a < b);
 }
 
 int main(void) {
-    const char *delims = " \t\r\n";
-
-    size_t line_buf_size = 0;
-    char *line_buf = NULL;
+    char *delims = " \t\r\n";
     int valid_count = 0;
-    while(getline(&line_buf, &line_buf_size, stdin) != -1){
-        char *words[WORD_LIMIT] = { NULL };
+
+    size_t len = 0;
+    char *line = NULL;
+    char **words = calloc(WORD_LIMIT, sizeof (char *));
+    while(getline(&line, &len, stdin) > 0) {
+        memset(words, 0, WORD_LIMIT * sizeof (char *));
         int i = 0;
-        char *tok = strtok(line_buf, delims);
+        char *tok = strtok(line, delims);
         do {
             words[i++] = tok;
             tok = strtok(NULL, delims);
         } while(tok !=NULL && i<WORD_LIMIT);
 
-        for(int i = 0; i<WORD_LIMIT; i++) {
-            if(words[i] == NULL) { break; }
-            qsort(words[i], strlen(words[i]), sizeof(*words[i]), charcmp);
+        for(int i = 0; i<WORD_LIMIT && words[i] != NULL; i++) {
+            qsort(words[i], strlen(words[i]), sizeof (*words[i]), charcmp);
             for(int j = 0; j < i; j++) {
                 if(!strcmp(words[i], words[j])) {
                     goto done;
@@ -36,9 +38,9 @@ int main(void) {
         valid_count++;
 
     done:
-        free(line_buf);
-        line_buf = NULL;
+        /* nop */
     }
+    free(line);
 
     printf("%d\n", valid_count);
     return 0;
