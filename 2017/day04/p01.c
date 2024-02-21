57bed0782e8f733bd2e24a0fae6199b3445ab568
Adam Mullins <adam.l.m@gmail.com>
2024-02-21 05:35:27 -0500

    [minor] cleanup day04

--- 2017/day04/p01.c
+++ 2017/day04/p01.c
@@ -1,39 +1,47 @@
-#include <string.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define WORD_LIMIT 32
 
 int main(void) {
-    const char *delims = " \t\r\n";
-
-    size_t line_buf_size = 0;
-    char *line_buf = NULL;
+    char *delims = " \t\r\n";
     int valid_count = 0;
-    while(getline(&line_buf, &line_buf_size, stdin) != -1){
-        char *words[WORD_LIMIT] = { NULL };
+
+    size_t len = 0;
+    char *line = NULL;
+    char **words = calloc(WORD_LIMIT, sizeof (char *));
+    while(getline(&line, &len, stdin) > 0){
+        // Read in line, split into char *words[]. We aren't
+        // allocating new memory here, we are just storing indicies
+        // into `line`.
+        memset(words, 0, WORD_LIMIT * sizeof (char *));
         int i = 0;
-        char *tok = strtok(line_buf, delims);
+        char *tok = strtok(line, delims);
         do {
             words[i++] = tok;
             tok = strtok(NULL, delims);
         } while(tok !=NULL && i<WORD_LIMIT);
 
-        for(int i = 0; i<WORD_LIMIT; i++) {
-            if(words[i] == NULL) { break; }
+        // Check every token against every other token in the string.
+        // if every token is unique, execution falls out of the
+        //   for() and valid_count is incremented.
+        // if there are two matching tokens, then we "break" by goto.
+        //   C does not have multi-level breaks.
+        for(int i = 0; i<WORD_LIMIT && words[i] != NULL; i++) {
             for(int j = 0; j < i; j++) {
                 if(!strcmp(words[i], words[j])) {
-                    goto done;
+                    goto next;
                 }
             }
         }
         valid_count++;
 
-    done:
-        free(line_buf);
-        line_buf = NULL;
+    next:
+        /* nop */
     }
+    free(line);
 
     printf("%d\n", valid_count);
     return 0;
