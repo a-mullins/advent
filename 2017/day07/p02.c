af4491253213eda62701f92bb99192b59d095f8a
Adam Mullins <adam.l.m@gmail.com>
2024-02-26 18:42:12 -0500

    [wip]

--- 2017/day07/p02.c
+++ 2017/day07/p02.c
@@ -5,6 +5,8 @@
 
 #define DELIMS " \t\r\n()->,"
 #define MAX_LINES 2048
+#define FIELD_WIDTH 16
+#define MAX_CHILDREN 8
 
 typedef struct prog {
     char *name;
@@ -14,7 +16,8 @@ typedef struct prog {
 } prog;
 
 int namecmp(const void *a, const void *b);
-int prognamecmp(const void *a, const void *b);
+ssize_t name2idx(char *needle);
+int tree_weight(int i);
 
 // When processing the list of programs, a program might make a
 // forward-reference by listing a child that hasn't yet appeared.
@@ -46,87 +49,145 @@ int prognamecmp(const void *a, const void *b);
 //
 // 3. Find root node: scan by col, empty col means no parents.
 //    That col corresponds to the root node of the tree.
+static char name[MAX_LINES][FIELD_WIDTH];
+static int weight[MAX_LINES];
+static int names_len = 0;
+static bool tree[MAX_LINES][MAX_LINES] = {false};
+
 int main(void) {
-    prog *progs = calloc(MAX_LINES, sizeof(prog));
-    memset(progs, 0, MAX_LINES * sizeof(prog));
-    char **names = calloc(MAX_LINES, sizeof(char *));
-    memset(names, 0, MAX_LINES * sizeof(char *));
-    int progs_len = 0;
-    int names_len = 0;
-
-    // progs[0].name = "foo";
-    // progs[1].name = "bar";
-    // progs[0].children = calloc(2, sizeof(prog *));
-    // memset(progs[0].children, 0, 2 * sizeof(prog *));
-    // progs[0].children[0] = &progs[1];
-
-    char **children = calloc(MAX_LINES, sizeof(char *));
-    memset(children, 0, MAX_LINES * sizeof(char *));
-    int children_len = 0;
-
     size_t len = 80;
     char *line = calloc(len, sizeof(char));
+
+    // first pass.
     while(getline(&line, &len, stdin) > 0) {
-        char *tok = strtok(line, DELIMS);
-        // program name
-        progs[progs_len].name = calloc(strlen(tok) + 1, sizeof(char));
-        strcpy(progs[progs_len].name, tok);
+        // name.
+        strncpy(name[names_len++], strtok(line, DELIMS), FIELD_WIDTH-1);
+    }
 
-        names[names_len] = calloc(strlen(tok) + 1, sizeof(char));
-        strcpy(names[names_len], tok);
-        // discard first field, which is weight
-        progs[progs_len].weight = atoi(strtok(NULL, DELIMS));
+    if(fseek(stdin, 0L, SEEK_SET) != 0) {
+        fprintf(stderr,
+                "seek failed. please connet stdin to a regular file.\n");
+        exit(1);
+    }
 
-        // collect names of children.
-        // We don't need to worry about repeated strings, because
-        // a child can only have one parent.
-        // ie, no child will show up in two different lines.
+    qsort(&name, names_len, FIELD_WIDTH, namecmp);
+
+    // second pass.
+    while(getline(&line, &len, stdin) > 0) {
+        char *tok = NULL;
+        ssize_t parent_i = -1;
+
+        // program name.
+        tok = strtok(line, DELIMS);
+        parent_i = name2idx(tok);
+
+        // weight
+        tok = strtok(NULL, DELIMS);
+        weight[parent_i] = atoi(tok);
+
+        ssize_t child_i = -1;
         while((tok = strtok(NULL, DELIMS)) != NULL) {
-            children[children_len] = calloc(strlen(tok) + 1, sizeof(char));
-            strcpy(children[children_len], tok);
-            children_len++;
+            child_i = name2idx(tok);
+            tree[parent_i][child_i] = true;
         }
-        //names_len++;
-        progs_len++;
     }
     free(line); line = NULL; len = 0;
 
-    //printf("%d\n", prognamecmp(progs, progs + 1));
-
-    // For every program, check if it is a child.
-    // If it is not a child of any node, then it must be the root.
-    qsort(progs, progs_len, sizeof(prog), prognamecmp);
-    // qsort(children, children_len, sizeof(*children), namecmp);
-    // for(int i = 0; i<MAX_LINES && names[i] != NULL; i++) {
-    //     char **result = bsearch(
-    //         &names[i], children, children_len,
-    //         sizeof(*children), namecmp);
-    //     if(result == NULL) {
-    //         printf("%s\n", names[i]);
-    //     }
-    // }
-    for(int i = 0; i<progs_len; i++) {
-        printf("%s\n", progs[i].name);
+    for(int i = 0; i<names_len; i++) {
+        printf("%4d: %8s (%5d)\n", i, name[i], weight[i]);
     }
 
-    // for(int i = 0; children[i] != NULL; i++) {free(children[i]);}
-    // free(children);
-    // for(int i = 0; names[i] != NULL; i++) {free(names[i]);}
-    // free(names);
-    // free(progs);
+    char *needle = "kcimi";
+    printf("%s is at idx %lu\n", needle, name2idx(needle));
+
+    // in the adjacency-matrix representation of a tree, if a col is
+    // all false, then that col represents a node that has no
+    // in-links, ie, no parents.
+    int root_idx = -1;
+    for(int col = 0; col<MAX_LINES; col++) {
+        bool rootp = true;
+        for(int row = 0; row<MAX_LINES; row++) {
+            if(tree[row][col] == true) {
+                rootp = false;
+                break;
+            }
+        }
+        if(rootp) {
+            root_idx = col;
+            break;
+        }
+    }
+    printf("root is at idx %d: %s\n", root_idx, name[root_idx]);
+
+    // find unbalanced node.
+    //printf("tree weight is %d\n", tree_weight(name2idx("pabgfh")));
+    printf("tree weight is %d\n", tree_weight(root_idx));
+
     return 0;
 }
 
 int namecmp(const void *a, const void *b) {
-    // a will be a pointer-to-pointer-to-char
-    const char *p = *(const char **)a;
-    const char *q = *(const char **)b;
-    return strcmp(p, q);
+    // a, b are pointers to char.
+    return strcmp(a, b);
 }
 
-int prognamecmp(const void *a, const void *b) {
-    // a, b will be pointers to struct prog
-    const prog *p = (const prog *)a;
-    const prog *q = (const prog *)b;
-    return strcmp(p->name, q->name);
+ssize_t name2idx(char *needle) {
+    void *offset = bsearch(needle, name, names_len, FIELD_WIDTH, namecmp);
+    if(offset == NULL) {
+        return (ssize_t)-1;
+    }
+
+    return (ssize_t)((offset - (void *)name) / FIELD_WIDTH);
+}
+
+// Since there is only one mismatched node, there will only ever be
+// one branch that we need to follow, unlike DFS/BFS/A*, etc, so an
+// iterative approach will work just as well/be just as easy to reason
+// about as the recursive one. Also, as an aside, this problem
+// probably is unlikely to benefit from parrallelization.
+//
+// 1. Is everything on this node even?
+//    a. If so, then it is the one whose weight needs to be
+//       adjusted. Calculate & return value.
+// 2. Examine branches of node of this node. Explore whichever node is
+//     mismatched.
+//
+// for child of children(cur):
+//     if children(child) are even:
+//        return difference of this child and any other child.
+int tree_weight(int root) {
+    int len = 0;
+    int child[MAX_CHILDREN];
+    for(int to = 0; to < MAX_LINES; to++) {
+        if(tree[root][to]) {
+            child[len++] = to;
+        }
+    }
+
+    // 1. if this node is balanced, stop.
+    // 2. if this node is unbalanced, ...
+
+    if(len > 0) {
+        int cur = tree_weight(child[0]);
+        for(int i=1; i<len; i++) {
+            if(cur != tree_weight(child[i])) {
+                printf("program %s's (id#%d) disc is unbalanced.\n",
+                       name[root],
+                       root);
+            }
+        }
+    }
+
+    // if(len > 0) {
+    //     printf("node %4d (%s) has children:\n", root, name[root]);
+    //     for(int i=0; i<len; i++) {
+    //         printf("\t%4d (%s)\n", child[i], name[child[i]]);
+    //     }
+    // } else {
+    //     printf("node %4d (%s) is a leaf\n", root, name[root]);
+    // }
+
+    // printf("tree_weight(%d) -> %d\n", root, sum);
+
+    return sum;
 }
