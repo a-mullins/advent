8552474abf608f12b11f9b86f8c9da9db08ca097
Adam Mullins <adam.l.m@gmail.com>
2024-12-30 01:18:40 -0500

    2017/day06: use dynamic array instead of VLA

--- 2017/day06/p01.c
+++ 2017/day06/p01.c
@@ -2,77 +2,104 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "darray.h"
+
+
+#define ARRAY_LEN(X) sizeof X / sizeof X[0]
+
+
+typedef struct memory {
+    int blocks[16];
+} memory;
+
+
+void
+get_input(darray *history)
+{
+    static const char *DELIMS = " \t\r\n";
+
+    char *line = NULL;
+    size_t len = 0;
+    if (getline(&line, &len, stdin) <= 0) {
+        fprintf(stderr, "ERR");
+        exit(1);
+    }
+
+    memory m = {0};
+    size_t block_cap = ARRAY_LEN(m.blocks);
+    size_t block_len = 0;
+    char *tok = strtok(line, DELIMS);
+    do
+    {
+        m.blocks[block_len++] = atoi(tok);
+    } while ((tok = strtok(NULL, DELIMS)) != NULL
+             && block_len < block_cap);
+
+    darray_push(history, &m);
+    free(line);
+
+    // samurai error handling: return successful, or don't return at all.
+    if(block_len != block_cap) {fprintf(stderr, "ERR\n"); exit(1);}
+}
 
 
 int
 main(void)
 {
-    const int HIST_SIZE = 4096;
-    const int BANKS = 16;
-    const char *DELIMS = " \t\r\n";
-    // TODO allocate me on the heap.
-    int hist[HIST_SIZE][BANKS] = {0};
-    int cur_state = 0;
+    darray history = {0};
+    darray_init(&history, sizeof (memory));
 
-    {
-        char *buf = NULL;
-        size_t len = 0;
-        if (getline(&buf, &len, stdin) <= 0) {
-            fprintf(stderr, "ERR");
-            exit(1);
-        }
+    get_input(&history);
 
-        char *tok = strtok(buf, DELIMS);
-        for (int i=0; tok != NULL; i++) {
-            hist[0][i] = atoi(tok);
-            tok = strtok(NULL, DELIMS);
-        }
-        free(buf);
-    }
+    // Generate states until we find a repeat (or run out of buffer).
+    while (1) {
+        memory m = {0};
+        memcpy(m.blocks, darray_last(&history), sizeof m.blocks);
 
-    // Generate states until we run out of buffer (or find a repeat).
-    for (int limit=0; limit<HIST_SIZE; limit++) {
-        // find biggest block count & index of
-        memcpy(&hist[cur_state+1], &hist[cur_state], sizeof(int) * BANKS);
-        cur_state++;
-        int blocks = 0;
-        int max_index = 0;
-        for (int i = 0; i < BANKS; i++) {
-            if (hist[cur_state][i] > blocks) {
-                max_index = i;
-                blocks = hist[cur_state][i];
+        // Find largest block's index and size.
+        int blocks = m.blocks[0];
+        int max_i = 0;
+        for(size_t i = 1; i < ARRAY_LEN(m.blocks); i++) {
+            if (m.blocks[i] > blocks) {
+                blocks = m.blocks[i];
+                max_i = i;
             }
         }
 
-        // remove blocks from bank, redistribute.
-        hist[cur_state][max_index] = 0;
-        int cur_index = max_index;
-        while (blocks > 0) {
-            cur_index = (cur_index+1) % BANKS;
-            hist[cur_state][cur_index]++;
-            blocks--;
+        // Set its size and redistribute it to the others.
+        m.blocks[max_i] = 0;
+        for (size_t i = max_i; blocks > 0; blocks--) {
+            i = (i+1) % (ARRAY_LEN(m.blocks));
+            m.blocks[i]++;
         }
 
-        // check if we have seen cur_state before.
-        for (int old_state=0; old_state<cur_state; old_state++) {
-            int match = false;
-            for (int i=0; i<BANKS; i++) {
-                match = true;
-                if (hist[old_state][i] != hist[cur_state][i]) {
+        // check if we have seen this state before
+        // ie, if (m in history);
+        bool match;
+        for (size_t i = 0; i < history.len; i++) {
+            match = true;
+            memory *old_m = (memory *)darray_get(&history, i);
+            for (size_t j = 0; j < ARRAY_LEN(m.blocks); j++) {
+                if (old_m->blocks[j] != m.blocks[j]) {
                     match = false;
                     break;
                 }
             }
             if (match) {
-                printf("match found, iter %d matches %d: ", cur_state, old_state);
-                for (int i=0; i<BANKS; i++) {
-                    printf("%d ", hist[cur_state][i]);
-                }
-                printf("\nloop size: %d\n", cur_state - old_state);
-                exit(0);
+                printf("old: %ld, new: %ld, diff: %ld\n",
+                       i,
+                       history.len,
+                       history.len-i);
+                goto exit;
             }
         }
+
+        // This memory state doesn't match a previous one.
+        // Push it on to the stack and try again.
+        darray_push(&history, &m);
     }
 
+exit:
+    darray_free(&history);
     return 0;
 }
