d5d972f88dd831630c309fc4518722354fdbdaa9
Adam Mullins <adam.l.m@gmail.com>
2024-12-30 01:18:40 -0500

    2017/day06: use dynamic array instead of VLA

---
+++ 2017/day06/darray.h
@@ -0,0 +1,27 @@
+// A simple dynamic array. Also known as a `vector` in the C++ world.
+#pragma once
+
+typedef struct darray {
+    size_t cap;
+    size_t len;
+    size_t elem_size;
+    void *buf;
+} darray;
+
+void
+darray_init(darray *d, size_t elem_size);
+
+void *
+darray_get(darray *d, size_t n);
+
+void *
+darray_last(darray *d);
+
+void
+darray_push(darray *d, void *elem);
+
+void
+darray_clear(darray *d);
+
+void
+darray_free(darray *d);
