5d6aed81ac504501e511e65ca07553fb098991d2
Adam Mullins <adam.l.m@gmail.com>
2025-04-27 17:25:35 -0400

    2017/day08 use static array instead of dynamic

--- 2017/day08/p01.c
+++ 2017/day08/p01.c
@@ -2,30 +2,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "../lib/darray.h"
+
+// To find array size:
+//   cut -d' ' -f1,5 <input | tr ' ' '\n' | sort | uniq | wc -l
+#define REGS_CAP 32
+#define NAME_MAX 12
 
 
-#define NAME_CAP 12
+typedef enum { GT, LT, GE, LE, EQ, NE } oper;
 
 
-typedef struct reg {
-    char name[NAME_CAP];
+typedef struct {
+    char name[NAME_MAX];
     int val;
 } reg;
 
 
-typedef enum oper { GT, LT, GE, LE, EQ, NE } oper;
-char oper_s[6][3] = {">", "<", ">=", "<=", "==", "!="};
-
-
-typedef struct cond {
+typedef struct {
     reg *reg;
     oper oper;
     int val;
 } cond;
 
 
-typedef struct instr {
+typedef struct {
     reg *target;
     bool inc;
     int amt;
@@ -33,89 +33,42 @@ typedef struct instr {
 } instr;
 
 
-int
-regcmp(const void *a, const void *b)
-{
-    return strcmp(((const reg *)a)->name, ((const reg *)b)->name);
-}
-
-
-void
-strtoinstr(char *s, instr *i, darray *registers)
-{
-    char *tok = strtok(s, " ");
-    i->target = darray_bsearch(registers, tok, regcmp);
-
-    tok = strtok(NULL, " ");
-    if(strcmp(tok, "inc") == 0) {
-        i->inc = true;
-    } else {
-        i->inc = false;
-    }
-
-    tok = strtok(NULL, " ");
-    i->amt = atoi(tok);
-
-    tok = strtok(NULL, " ");
-    /* discard */
-
-    tok = strtok(NULL, " ");
-    i->cond.reg = darray_bsearch(registers, tok, regcmp);
-
-    tok = strtok(NULL, " ");
-    if(!strcmp(tok, ">"))  {i->cond.oper = GT;}
-    if(!strcmp(tok, "<"))  {i->cond.oper = LT;}
-    if(!strcmp(tok, ">=")) {i->cond.oper = GE;}
-    if(!strcmp(tok, "<=")) {i->cond.oper = LE;}
-    if(!strcmp(tok, "==")) {i->cond.oper = EQ;}
-    if(!strcmp(tok, "!=")) {i->cond.oper = NE;}
-
-    tok = strtok(NULL, " ");
-    i->cond.val = atoi(tok);
-}
-
-
-int
-max_reg(darray *regs)
-{
-    int max = ((reg *)darray_get(regs, 0))->val;
-    for (size_t i = 1; i < regs->len; i++) {
-        int other = ((reg *)darray_get(regs, i))->val;
-        if (other > max) {max = other;}
-    }
-    return max;
-}
+static int regcmp(const void *a, const void *b);
+static void strtoinstr(char *s, instr *i, reg *registers, size_t len);
+static int max_reg(reg *registers, size_t len);
 
 
 int
 main(void)
 {
-    darray regs;
-    darray_init(&regs, sizeof (reg));
+    size_t regs_len = 0;
+    reg regs[REGS_CAP] = {0};
 
     if(fseek(stdin, 0, SEEK_SET) != 0) {
-        fprintf(stderr, "ERR\n"); exit(1);
+        fprintf(stderr, "ERR stdin not seekable\n");
+        exit(1);
     }
 
-    char *line = NULL;
-    size_t len = 0;
-    while (getline(&line, &len, stdin) >= 0) {
+    // Collect register names.
+    char line[128] = {0};
+    while (fgets(line, 128, stdin)) {
         reg r = {0};
-        char *tok = strtok(line, " ");
-        strncpy(r.name, tok, NAME_CAP-1);
-        if(darray_in(&regs, &r) == -1) {
-            darray_push(&regs, &r);
+        line[strcspn(line, " ")] = '\0';
+        strcpy(r.name, line);
+
+        // Seen this name already?
+        bool seen = bsearch(&r, regs, regs_len, sizeof (reg), regcmp);
+        if (!seen) {
+            regs[regs_len++] = r;
+            qsort(regs, regs_len, sizeof (reg), regcmp);
         }
     }
-    free(line); line = NULL; len = 0;
 
-    // So we can use darray_bsearch later.
-    darray_qsort(&regs, regcmp);
     rewind(stdin);
     int all_time_high = 0;
-    while (getline(&line, &len, stdin) >= 0) {
+    while (fgets(line, 128, stdin)) {
         instr i = {0};
-        strtoinstr(line, &i, &regs);
+        strtoinstr(line, &i, regs, regs_len);
 
         reg *cond_lhs = i.cond.reg;
         bool test = false;
@@ -146,14 +99,72 @@ main(void)
                 i.target->val -= i.amt;
             }
         }
-        int cur_max = max_reg(&regs);
+        int cur_max = max_reg(regs, regs_len);
         if (cur_max > all_time_high) {all_time_high = cur_max;}
     }
-    free(line); line = NULL; len = 0;
 
-    printf("all time high: %d, max at end: %d\n", all_time_high, max_reg(&regs));
+    printf("all time high: %d, max at end: %d\n",
+           all_time_high,
+           max_reg(regs, regs_len));
 
-    // Cleanup.
-    darray_free(&regs, NULL);
     return 0;
+}
+
+
+int
+regcmp(const void *a, const void *b)
+{
+    return strcmp(((const reg *)a)->name, ((const reg *)b)->name);
+}
+
+
+void
+strtoinstr(char *s, instr *i, reg *registers, size_t len)
+{
+    reg r = {0};
+    char *tok = strtok(s, " ");
+    strcpy(r.name, tok);
+    //i->target = darray_bsearch(registers, tok, regcmp);
+    i->target = bsearch(&r, registers, len, sizeof (reg), regcmp);
+
+    tok = strtok(NULL, " ");
+    if(strcmp(tok, "inc") == 0) {
+        i->inc = true;
+    } else {
+        i->inc = false;
+    }
+
+    tok = strtok(NULL, " ");
+    i->amt = atoi(tok);
+
+    tok = strtok(NULL, " ");
+    /* discard */
+
+    tok = strtok(NULL, " ");
+    strcpy(r.name, tok);
+    i->cond.reg = bsearch(&r, registers, len, sizeof (reg), regcmp);
+    //darray_bsearch(registers, tok, regcmp);
+
+    tok = strtok(NULL, " ");
+    if(!strcmp(tok, ">"))  {i->cond.oper = GT;}
+    if(!strcmp(tok, "<"))  {i->cond.oper = LT;}
+    if(!strcmp(tok, ">=")) {i->cond.oper = GE;}
+    if(!strcmp(tok, "<=")) {i->cond.oper = LE;}
+    if(!strcmp(tok, "==")) {i->cond.oper = EQ;}
+    if(!strcmp(tok, "!=")) {i->cond.oper = NE;}
+
+    tok = strtok(NULL, " ");
+    i->cond.val = atoi(tok);
+}
+
+
+int
+max_reg(reg *registers, size_t len)
+{
+    int max = registers[0].val;
+    for (size_t i = 1; i < len; i++) {
+        int other = registers[i].val;
+        if (other > max) {max = other;}
+    }
+    return max;
 }
