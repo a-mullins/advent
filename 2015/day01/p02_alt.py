f7c354d351b2a59f31f0d56e3e7d4e298c940935
Adam Mullins <adam.l.m@gmail.com>
2025-07-03 03:51:36 -0400

    [minor] disable flake8 for 2015/day01/p02_alt.py

--- 2015/day01/p02_alt.py
+++ 2015/day01/p02_alt.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
-# Advent of Code 2015 Day 2, One-Liner
+# flake8: noqa
 from collections import deque
 from itertools import accumulate, count, takewhile
-
-#print(deque(takewhile(lambda x: x[0] > -2, accumulate(input(), lambda tot, next: (tot[0] + 1, tot[1] + 1) if next == "(" else (tot[0] - 1, tot[1] + 1), initial=(0, 0))), maxlen=1)[0][1])  # noqa
 
 print(deque(takewhile(lambda x: x[0] > -2, accumulate(zip((1 if c == '(' else -1 for c in input()), count(start=1)), lambda tot, next: (tot[0] + next[0], next[1]))), maxlen=1)[0][1])
