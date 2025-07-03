927f1be685da91edab814146be37037c7d387629
Adam Mullins <adam.l.m@gmail.com>
2025-07-03 04:02:59 -0400

    [minor] make 2015/day01/p02_alt.py a true one-liner

--- 2015/day01/p02_alt.py
+++ 2015/day01/p02_alt.py
@@ -1,6 +1,3 @@
 #!/usr/bin/env python3
 # flake8: noqa
-from collections import deque
-from itertools import accumulate, count, takewhile
-
-print(deque(takewhile(lambda x: x[0] > -2, accumulate(zip((1 if c == '(' else -1 for c in input()), count(start=1)), lambda tot, next: (tot[0] + next[0], next[1]))), maxlen=1)[0][1])
+from collections import deque;from itertools import accumulate, count, takewhile;print(deque(takewhile(lambda x: x[0] > -2, accumulate(zip((1 if c == '(' else -1 for c in input()), count(start=1)), lambda tot, next: (tot[0] + next[0], next[1]))), maxlen=1)[0][1])
