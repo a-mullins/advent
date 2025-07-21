e8b170d7b15efe23a6c3c39ef11c41c087869449
Adam Mullins <adam.l.m@gmail.com>
2025-07-21 18:32:45 -0400

    [minor] spelling in comments

--- 2015/day01/p01_alt.py
+++ 2015/day01/p01_alt.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # Copyright (c) 2025 Adam Mullins
 #
-# Altenatine Solution for Advent of Code 2015, Day 01, Part 1
+# Alternative Solution for Advent of Code 2015, Day 01, Part 1
 #   https://adventofcode.com/2015/day/1
 
 print(sum((1 if c == "(" else -1 for c in input())))
