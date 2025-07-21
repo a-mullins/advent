7fe750fa1be1f2cdfc4f911cd8dd84b0b162471a
Adam Mullins <adam.l.m@gmail.com>
2025-07-21 19:14:36 -0400

    [minor] update explanations & formatting 2017/day04/p01{,_alt}.c

--- 2017/day04/p01.c
+++ 2017/day04/p01.c
@@ -19,15 +19,14 @@ main(void)
     char *words[WORDS_MAX] = {0};
     char line[128];
     while (fgets(line, 128, stdin)){
-        // Read in line, split into char *words[]. We aren't
-        // allocating new memory here, we are just storing indicies
-        // into `line`.
+        // Read in line, split into char *words[]. No new memory is
+        // malloc'd here, we are only storing pointers into `line`.
         short words_len = 0;
         char *tok = strtok(line, DELIMS);
         do {
             words[words_len++] = tok;
             tok = strtok(NULL, DELIMS);
-        } while (tok !=NULL && words_len<WORDS_MAX);
+        } while (tok != NULL && words_len < WORDS_MAX);
 
         // Check every token against every other token in the string.
         //
@@ -39,14 +38,14 @@ main(void)
         for (short i = 1; i < words_len; i++) {
             for (short j = 0; j < i; j++) {
                 if (!strcmp(words[i], words[j])) {
-                    goto next;
+                    goto not_valid;
                 }
             }
         }
         valid_count++;
 
-    next:
-        /* nop */;
+    not_valid:
+        ; /* nop */
     }
 
     printf("%d\n", valid_count);
--- 2017/day04/p01_alt.c
+++ 2017/day04/p01_alt.c
@@ -1,7 +1,18 @@
 // Copyright (c) 2025 Adam Mullins
 //
-// Solution for Advent of Code 2017, Day 04, Part 1
+// Alternative Solution for Advent of Code 2017, Day 04, Part 1
 //   https://adventofcode.com/2017/day/4
+//
+// In the other solution, every word in a line's uniqueness is checked
+// by comparing it to every other word in that line. That is something
+// like O(n^2 - n), I think.
+//
+// By comparison, this solution sorts the list by using stdlib's qsort
+// and then comparing adjacents for O(n log n + n).
+//
+// Of course when n = 512, as it does with the provided input, the
+// difference is likely so negligible that it's difficult to even
+// measure empirically with /usr/bin/time.
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -33,8 +44,9 @@ main(void)
         qsort(words, (size_t)words_len, sizeof (*words), wordcmp);
 
         for (short i = 1; i < words_len; i++) {
-            if (!strcmp(words[i], words[i-1]))
+            if (!strcmp(words[i], words[i-1])) {
                 goto next_line;
+            }
         }
         valid_count++;
 
