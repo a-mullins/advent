3268d6df6edddd009c0f2ce664ce7e181bd5f39b
Adam Mullins <adam.l.m@gmail.com>
2025-12-03 20:40:14 -0500

    clean up 2025/day02/p01

--- 2025/day02/p01.c
+++ 2025/day02/p01.c
@@ -17,62 +17,38 @@ static_assert(ULONG_MAX > ((unsigned long)1 << 33),
               "ERR: Unsigned long is less than 33 bits on this platform.");
 
 
-bool is_valid_id(unsigned long);
-
-
 int
 main(void)
 {
-    enum reader_state { READ_START, READ_END } state = READ_START;
-
+    unsigned long invalid_id_sum = 0;
     unsigned long start;
     unsigned long end;
-    unsigned long invalid_id_sum = 0;
     char s[16] = {'\0'};
     int n;
-    while (EOF != (n = fgetc(stdin))) {
+    while (EOF != (n = getchar())) {
         char c = (char)n;
-        if (state == READ_START && c == '-') {
-            start = (unsigned long)atol(s);
-            s[0] = '\0';
-            state = READ_END;
-        } else if (state == READ_END && c == ',') {
-            end = (unsigned long)atol(s);
 
-            for (unsigned long id = start; id <= end; id++) {
-                if (!is_valid_id(id)) {
-                    invalid_id_sum += (unsigned long)id;
-                    printf("invalid id %ld\n", id);
-                }
-            }
-
-            start = 0;
-            end = 0;
-            s[0] = '\0';
-            state = READ_START;
-        } else {
+        if (c != '-' && c != ',') {
             strncat(s, &c, 1);
+            continue;
         }
+
+        if (c == '-')
+            start = (unsigned long)atol(s);
+        if (c == ',') {
+            end = (unsigned long)atol(s);
+            for (unsigned long id = start; id <= end; id++) {
+                sprintf(s, "%ld", id);
+                size_t len = strlen(s);
+                if (len %2 == 1)
+                    continue;
+                if (!strncmp(s, s + ((len+1)/2), (len+1)/2))
+                    invalid_id_sum += (unsigned long)id;
+            }
+        }
+        s[0] = '\0';
     }
 
-    printf("sum %ld\n", invalid_id_sum);
+    printf("%ld\n", invalid_id_sum);
     return 0;
-}
-
-
-bool
-is_valid_id(unsigned long id)
-{
-    char s[16] = {'\0'};
-    //char *s_p = &s[0];
-    sprintf(s, "%ld", id);
-    size_t len = strlen(s);
-    // single numbers are always valid, odd lengths can't have sequences
-    // repeated exactly twice.
-    if (len % 2 == 1) return true;
-
-    if (!strncmp(s, s + ((len+1)/2), (len+1)/2))
-        return false;
-    
-    return true;
 }
