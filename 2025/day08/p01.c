db9e72fe4bb7534105ae99e771ba7823c925e42d
Adam Mullins <adam.l.m@gmail.com>
2025-12-11 17:38:06 -0500

    solve 2025/day08/p01

--- 2025/day08/p01.c
+++ 2025/day08/p01.c
@@ -1,73 +1,127 @@
-#include <float.h>
 #include <math.h>
+#include <stdbool.h>
 #include <stdio.h>
-#include "../lib/darray.h"
+#include <stdlib.h>
 
 
-typedef struct {
-    long x;
-    long y;
-    long z;
-} vec3;
+// The input has 1000 nodes. To make an adajency matrix, we will need to
+// allocate an array of 1000 * 1000 elements. We might as well round up to
+// 1024, since memory should usually be allocated in 2^n chunks anyway. If we
+// use the standard bool type, that means 1024 * 1024 * 1 Byte = 1 Mebibyte,
+// which is managable. On the other hand, if we do some bit packing use only a
+// single bit to mark when an edge exists, we can reduce this memory to 1/8th,
+// or just 128 KiB.
 
 
-float dist(vec3 u, vec3 v);
-//ssize_t find_circuit_by_pos(struct darray *circuits, vec3 pos);
-// boxes :: [vec3, vec3, ...],
-// pairs :: [(vec3, vec3), (vec3, vec3)...]
-// pairs is an out-param.
-void find_n_closest_pairs(struct darray *boxes, struct darray *pairs, size_t n);
-void print_circuit(struct darray *d);
-void add_box_to_target_circuits(struct darray *circuits, vec3 target, vec3 box);
-void circuits_free(void *d);
+typedef struct {long x; long y; long z;} vec3;
+typedef struct {vec3 v0; vec3 v1; float d;} threeple; // 1-ple, tuple, 3-ple.
+
+
+static vec3 box[1000] = {0};
+static size_t box_len = 0;
+static bool connected[1000] = {false};
+threeple pair[499'500] = {0}; // Combinations(1000,2) = 499,500.
+static size_t pair_len = 0;
+static bool adjacent[1000][1000] = {0};
+static long connected_component_size[1000];
+
+
+int threeplecmp(const void *a, const void*b);
+inline static float dist(vec3 u, vec3 v);
+inline static ssize_t get_id_by_vec3(vec3 v);
+void bfs(size_t root_id, bool seen[1000]);
+int revlongcmp(const void *a, const void *b);
 
 
 int
-main(void)
+main(int argc, char **argv)
 {
-    struct darray *boxes = darray_new(sizeof (vec3)); // this could be array.
-    struct darray *pairs = darray_new(sizeof (vec3) * 2);
-    struct darray *circuits = darray_new(sizeof (struct darray *));
-
-
     char line[128];
-    while (NULL != fgets(line, 128, stdin)) {
+    for (/*nop*/; NULL != fgets(line, 128, stdin); box_len++) {
         vec3 v;
         sscanf(line, "%ld,%ld,%ld", &v.x, &v.y, &v.z);
-        darray_push(boxes, &v);
-    }
-    struct darray *unconnected = darray_copy(boxes);
-    // List of unconnected boxes.
-    // printf("%ld junctions unconnected:\n", darray_len(boxes));
-    // print_circuit(boxes);
-    // puts("");
-
-    find_n_closest_pairs(boxes, pairs, 10);
-    for (size_t i = 0; i<10; i++) {
-        vec3 *v_p = darray_get(pairs, i);
-        add_box_to_target_circuits(circuits, v_p[0], v_p[1]);
-        darray_del_val(unconnected, &(v_p[0]));
-        darray_del_val(unconnected, &(v_p[1]));
+        box[box_len] = v;
     }
 
-    // List of unconnected boxes.
-    printf("%ld junctions unconnected:\n", darray_len(unconnected));
-    print_circuit(unconnected);
+    // for (int i = 0; i<box_len; i++)
+    //     printf("%5ld %5ld %5ld\n", box[i].x, box[i].y, box[i].z);
+
+    for (size_t i = 0; i<box_len; i++) {
+        for (size_t j = i; j<box_len; j++) {
+            if (i == j) continue;
+            // printf("i = %3d, j = %3d, res = %3d | ", i, j, pair_len);
+            pair[pair_len++] = (threeple){box[i], box[j], dist(box[i], box[j])};
+            // printf("box[i] = %3d %3d %3d, box[j] = %5d %5d %5d, d = %.2f\n",
+            //        box[i].x, box[i].y, box[i].z,
+            //        box[j].x, box[j].y, box[j].z,
+            //        dist(box[i], box[j]));
+        }
+    }
+
+    qsort(pair, pair_len, sizeof (pair[0]), threeplecmp);
+
+    puts("-=[ 1000 Closest Junction Boxes ]=-------"
+         "---------------------------------------");
+    for (size_t i = 0; i<1000; i++) {
+        printf("pair %3d: v0 = [%5d %5d %5d], v1 = [%5d %5d %5d], d = %.2f\n",
+               i,
+               pair[i].v0.x, pair[i].v0.y, pair[i].v0.z,
+               pair[i].v1.x, pair[i].v1.y, pair[i].v1.z,
+               pair[i].d);
+        ssize_t v0_id = get_id_by_vec3(pair[i].v0);
+        ssize_t v1_id = get_id_by_vec3(pair[i].v1);
+        printf("         v0_id: %-18ld v1_id: %ld\n\n", v0_id, v1_id);
+
+        adjacent[v0_id][v1_id] = true;
+        adjacent[v1_id][v0_id] = true;
+        connected[v0_id] = true;
+        connected[v1_id] = true;
+    }
+
+    puts("-=[ Adjacency Matrix ]=----------------"
+         "---------------------------------------");
+    puts("  01234567890123456789\n"
+         " +--------------------");
+    for (size_t i = 0; i < 20; i++) {
+        printf("%d|", i % 10);
+        for (size_t j = 0; j < 20; j++) {
+            printf("%s", adjacent[i][j] ? "*" : ".");
+        }
+        puts("");
+    }
+
     puts("");
-    printf("%ld non-singleton circuits.\n", darray_len(circuits));
-    for (size_t i = 0; i < darray_len(circuits); i++) {
-        struct darray *circ = *(struct darray **)darray_get(circuits, i);
-        printf("circuit %ld has %ld boxes:\n", i, darray_len(circ));
-        print_circuit(circ);
-    }
+    bool graph_seen[1000] = {false};
+    for (size_t i = 0; i<1000; i++)
+        if (/* connected[i] && */ !graph_seen[i]) {
+            printf("-=[ Connected Component from %02ld ]=-----"
+                   "---------------------------------------\n", i);
+            bool component_seen[1000] = {false};
+            bfs(i, component_seen);
+            long size = 0;
+            for (size_t j = 0; j < 1000; j++) {
+                if (component_seen[j]) size++;
+                graph_seen[j] |= component_seen[j];
+            }
+            connected_component_size[i] = size;
+            printf("Circuit has %ld boxes.\n", size);
+        }
 
-    darray_free(circuits, circuits_free);
-    darray_free(pairs, NULL);
-    darray_free(boxes, NULL);
+    qsort(connected_component_size, 1000,
+          sizeof (connected_component_size[0]), revlongcmp);
+
+    for(int i = 0; i<3; i++)
+        printf("%ld\n", connected_component_size[i]);
+    puts("");
+
+    printf("%ld\n", connected_component_size[0]
+                    * connected_component_size[1]
+                    * connected_component_size[2]);
     return 0;
 }
 
 
+inline static
 float
 dist(vec3 u, vec3 v)
 {
@@ -78,149 +132,55 @@ dist(vec3 u, vec3 v)
 }
 
 
-// ssize_t
-// find_circuit_by_pos(struct darray *circuits, vec3 pos)
-// {
-//     for (ssize_t i = 0; i < darray_len(circuits); i++)
-//         if (darray_in(*(struct darray**)darray_get(circuits, i), pos) >= 0)
-//             return i;
-//     return -1;
-// }
-
-
-// boxes :: [vec3, vec3, ...],
-// pairs :: [(vec3, vec3), (vec3, vec3)...], ie, [vec3 *, vec3 *]
-// pairs is an out-param.
-void
-find_n_closest_pairs(struct darray *boxes, struct darray *pairs, size_t n)
+int
+threeplecmp(const void *a, const void*b)
 {
-    for (size_t i = 0; i < n; i++) {
-        vec3 v0, v1;
-        float min = FLT_MAX;
-        float lower_bound;
-        if (darray_last(pairs)) {
-            vec3 *v_p0 = darray_last(pairs);
-            lower_bound = dist(v_p0[0], v_p0[1]);
-        } else {
-            lower_bound = 0.0f;
-        }
-        for (size_t i = 0; i<darray_len(boxes); i++) {
-            for (size_t j = i + 1; j<darray_len(boxes); j++) {
-                vec3 maybe0 = *(vec3 *)darray_get(boxes, i);
-                vec3 maybe1 = *(vec3 *)darray_get(boxes, j);
-                float d = dist(maybe0, maybe1);
-                if (d > lower_bound && d < min) {
-                    v0 = maybe0;
-                    v1 = maybe1;
-                    min = d;
-                }
-            }
-        }
-        vec3 v[2] = {v0, v1};
-        darray_push(pairs, v);
-    }
-    return;
+    threeple *A = (threeple *)a;
+    threeple *B = (threeple *)b;
+
+    if (A->d < B->d)
+        return -1;
+    if (A->d > B->d)
+        return 1;
+    return 0;
 }
 
 
-// circuits :: [darray *, darray *, ...], equivalently,
-// circuits :: [[vec3], [vec3]]
-// remember that darray_get() returns a pointer to the elem, so do
-// darray *circ = *(darray **)darray_get(circuits, ...)
-void
-add_box_to_target_circuits(struct darray *circuits, vec3 target, vec3 box)
+inline static
+ssize_t
+get_id_by_vec3(vec3 v)
 {
-    struct darray *target_circ = NULL;
-    for (ssize_t i = 0; i < darray_len(circuits); i++) {
-        struct darray *tmp = *(struct darray**)darray_get(circuits, i);
-        if (darray_in(tmp, &target) >= 0)
-            target_circ = tmp;
-    }
-    if (target_circ) {
-        darray_push(target_circ, &box);
-    } else {
-        target_circ = darray_new(sizeof (vec3));
-        darray_push(target_circ, &target);
-        darray_push(target_circ, &box);
-        darray_push(circuits, &target_circ);
-    }
-
-    return;
-}
-
-// void
-// join_closest_boxes(struct darray *unconnected, struct darray *circuits)
-// {
-//     size_t a, b;
-//     float min = FLT_MAX;
-//     for (size_t i = 0; i<darray_len(unconnected); i++) {
-//         for (size_t j = i + 1; j<darray_len(unconnected); j++) {
-//             float d = dist(*(vec3 *)darray_get(unconnected, i),
-//                            *(vec3 *)darray_get(unconnected, j));
-//             if (d < min) {
-//                 a = i;
-//                 b = j;
-//                 min = d;
-//             }
-//         }
-//     }
-//     vec3 v0;
-//     vec3 v1;
-
-
-//     // printf("%ld and %ld are closest at %.1f units.\n", a, b, min);
-//     // // array of pointers to dynamic arrays.
-//     // struct darray *circ = darray_new(sizeof (vec3));
-//     // vec3 v0 = *(vec3 *)darray_get(unconnected, a);
-//     // vec3 v1 = *(vec3 *)darray_get(unconnected, b);
-//     // darray_push(circ, &v0);
-//     // darray_push(circ, &v1);
-//     // darray_push(circuits, &circ);
-//     // // darray_del_id(unconnected, darray_in(unconnected, &v0));
-//     // // darray_del_id(unconnected, darray_in(unconnected, &v1));
-//     // darray_del_val(unconnected, &v0);
-//     // darray_del_val(unconnected, &v1);
-
-//     // printf("%05ld %05ld %05ld is in circuit %ld\n",
-//     //        v0.x, v0.y, v0.z,
-//     //        find_circuit_by_pos(circuits, &v0));
-//     // printf("%05ld %05ld %05ld is in circuit %ld\n",
-//     //        v1.x, v1.y, v1.z,
-//     //        find_circuit_by_pos(circuits, &v1));
-
-//     // printf("\ncircuit 0 has elems\n");
-//     // for (size_t i = 0; i<darray_len(circ); i++) {
-//     //     vec3 *v_p = (vec3 *)darray_get(circ, i);
-//     //     printf("%05ld %05ld %05ld\n", v_p->x, v_p->y, v_p->z);
-//     // }
-// }
-
-
-// ssize_t
-// find_circuit_by_box(struct circuit_darr *circuits, vec3 box)
-// {
-//     // for vec3_darr in circuits...
-//     for (size_t i = 0; i < circuit_darr_len(circuits); i++)
-//         // is this box in this vec3_darr?
-//         if (vec3_darr_in(circuit_darr_get(circuits, i), box) > -1)
-//             return i;
-//     return -1;
-// }
-
-
-void
-print_circuit(struct darray *d)
-{
-    for (size_t i = 0; i<darray_len(d); i++) {
-        vec3 *v_p = (vec3 *)darray_get(d, i);
-        printf("%5ld %5ld %5ld\n", v_p->x, v_p->y, v_p->z);
-    }
+    for (size_t i = 0; i < box_len; i++)
+        if (box[i].x == v.x && box[i].y == v.y && box[i].z == v.z)
+            return i;
+    return -1;
 }
 
 
 void
-circuits_free(void *d)
+bfs(size_t root_id, bool seen[1000])
 {
-    darray_free(d, NULL);
-    return;
+    if (!seen) {
+        fprintf(stderr, "ERR seen must not be NULL\n");
+        return;
+    }
+
+    printf("%2d [%5ld %5ld %5ld]\n", root_id,
+           box[root_id].x, box[root_id].y, box[root_id].z);
+    seen[root_id] = true;
+    for (size_t to = 0; to<1000; to++)
+        if (adjacent[root_id][to])
+            if (!seen[to])
+                bfs(to, seen);
+}
+
+
+int
+revlongcmp(const void *a, const void *b) {
+    long n = *(int *)a;
+    long m = *(int *)b;
+
+    if(n < m) return 1;
+    if(n > m) return -1;
+    return 0;
 }
