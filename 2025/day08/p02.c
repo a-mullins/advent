7ca72f2af9643f50c2eb1c3d0de2aad97b7242db
Adam Mullins <adam.l.m@gmail.com>
2025-12-11 19:53:13 -0500

    2025/day08/p02 modify struct threeple to reduce memory usage
    was: struct {vec3, vec3, float}         -> 28 bytes
    now: struct {uint16_t, uint16_t, float} -> 8 Bytes
    
    These structs are stored in an array of size 499'500, so the
    overall savings is about 10MiB.

--- 2025/day08/p02.c
+++ 2025/day08/p02.c
@@ -1,18 +1,23 @@
-#include <math.h>
-#include <stdbool.h>
+#include <math.h>   // powf, sqrtf
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdint.h> // uint32_t
+#include <stdlib.h> // qsort
 
 
-typedef struct {long x; long y; long z;} vec3;
-typedef struct {vec3 v0; vec3 v1; float d;} threeple; // 1-ple, tuple, 3-ple.
+#define   SETBIT(A,k) (A[(k/32)] |=  (1<<(k%32)))
+#define UNSETBIT(A,k) (A[(k/32)] &= ~(1<<(k%32)))
+#define  TESTBIT(A,k) (A[(k/32)] &   (1<<(k%32)))
+
+
+typedef struct {uint32_t x; uint32_t y; uint32_t z;} vec3;
+typedef struct {uint16_t id0; uint16_t id1; float d;} threeple; // 1-ple, tuple, 3-ple.
 
 
 static vec3 box[1000] = {0};
 static size_t box_len = 0;
 threeple pair[499'500] = {0}; // Combinations(1000,2) = 499,500.
 static size_t pair_len = 0;
-static bool connected[1000] = {false};
+static uint32_t connected[32] = {0}; // 32 * (32 bit) = 1024 bits = 128 bytes.
 
 
 int threeplecmp(const void *a, const void*b);
@@ -21,12 +26,12 @@ inline static ssize_t get_id_by_vec3(vec3 v);
 
 
 int
-main(int argc, char **argv)
+main(void)
 {
     char line[128];
     while (box_len<1000 && NULL != fgets(line, 128, stdin)) {
         vec3 v;
-        sscanf(line, "%ld,%ld,%ld", &v.x, &v.y, &v.z);
+        sscanf(line, "%d,%d,%d", &v.x, &v.y, &v.z);
         box[box_len++] = v;
     }
 
@@ -34,7 +39,8 @@ main(int argc, char **argv)
     for (size_t i = 0; i<box_len; i++)
         for (size_t j = i; j<box_len; j++) {
             if (i == j) continue;
-            pair[pair_len++] = (threeple){box[i], box[j], dist(box[i], box[j])};
+            pair[pair_len++] =
+                (threeple){(uint16_t)i, (uint16_t)j, dist(box[i], box[j])};
         }
 
     // Sort by distance.
@@ -43,20 +49,20 @@ main(int argc, char **argv)
     // For every pair of junction boxes, from closest to most distant...
     for (size_t i = 0; i<pair_len; i++) {
         // ...connect the pair...
-        ssize_t v0_id = get_id_by_vec3(pair[i].v0);
-        ssize_t v1_id = get_id_by_vec3(pair[i].v1);
-        connected[v0_id] = true;
-        connected[v1_id] = true;
+        ssize_t v0_id = (ssize_t)pair[i].id0;
+        ssize_t v1_id = (ssize_t)pair[i].id1;
+        SETBIT(connected, v0_id);
+        SETBIT(connected, v1_id);
 
         // ...determine if all boxes are connected...
-        bool all_connected = true;
+        int all_connected = 1;
         for (size_t i = 0; i < box_len; i++)
-            all_connected &= connected[i];
+            all_connected &= TESTBIT(connected, i) ? 1 : 0;
 
         // ...and if so, report product of the x coords
         // of the last connected pair.
         if (all_connected) {
-            printf("%ld\n", box[v0_id].x * box[v1_id].x);
+            printf("%d\n", box[v0_id].x * box[v1_id].x);
             break;
         }
     }
@@ -81,20 +87,7 @@ threeplecmp(const void *a, const void*b)
     threeple *A = (threeple *)a;
     threeple *B = (threeple *)b;
 
-    if (A->d < B->d)
-        return -1;
-    if (A->d > B->d)
-        return 1;
+    if (A->d < B->d) return -1;
+    if (A->d > B->d) return 1;
     return 0;
-}
-
-
-inline static
-ssize_t
-get_id_by_vec3(vec3 v)
-{
-    for (size_t i = 0; i < box_len; i++)
-        if (box[i].x == v.x && box[i].y == v.y && box[i].z == v.z)
-            return i;
-    return -1;
 }
