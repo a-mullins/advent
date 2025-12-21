f5b312e0401ec88f4c579a935de8442a38d81830
Adam Mullins <adam.l.m@gmail.com>
2025-12-21 10:15:33 -0500

    2025/day09/p01 remove stdint.h include

--- 2025/day09/p01.c
+++ 2025/day09/p01.c
@@ -1,15 +1,14 @@
-#include <stdint.h>
 #include <stdio.h>
 
 
-typedef struct {uint32_t x; uint32_t y;} vec2;
+typedef struct {unsigned long x; unsigned long y;} vec2;
 
 
 vec2 tile[512] = {0};
-uint16_t tile_len = 0;
+unsigned int tile_len = 0;
 
 
-uint64_t area(vec2 v0, vec2 v1);
+unsigned long long area(vec2 v0, vec2 v1);
 
 int
 main(void)
@@ -17,26 +16,26 @@ main(void)
     char line[128];
     while (tile_len<512 && NULL != fgets(line, 128, stdin)) {
         vec2 v;
-        sscanf(line, "%d,%d", &v.x, &v.y);
+        sscanf(line, "%ld,%ld", &v.x, &v.y);
         tile[tile_len++] = v;
     }
 
-    uint64_t A = 0;
-    for (uint16_t i = 0; i<tile_len; i++)
-        for (uint16_t j = i; j<tile_len; j++) {
+    unsigned long long A = 0;
+    for (unsigned int i = 0; i<tile_len; i++)
+        for (unsigned int j = i; j<tile_len; j++) {
             if (i == j) continue;
             A = A > area(tile[i], tile[j]) ? A : area(tile[i], tile[j]);
         }
     
-    printf("%ld\n", A);
+    printf("%lld\n", A);
     return 0;
 }
 
 
-uint64_t
+unsigned long long
 area(vec2 v0, vec2 v1)
 {
-    uint64_t h = 1 + (v0.y > v1.y  ? v0.y - v1.y : v1.y - v0.y);
-    uint64_t w = 1 + (v0.x >= v1.x ? v0.x - v1.x : v1.x - v0.x);
+    unsigned long long h = 1 + (v0.y > v1.y  ? v0.y - v1.y : v1.y - v0.y);
+    unsigned long long w = 1 + (v0.x >= v1.x ? v0.x - v1.x : v1.x - v0.x);
     return h*w;
 }
